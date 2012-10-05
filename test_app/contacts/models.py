419e9896f725fe9751dfd59b22df9ff04e1b0244
Tom Tang <tly1980@gmail.com>
2012-10-05 14:29:44 +1000

    implementation of test apps

--- test_app/contacts/models.py
+++ test_app/contacts/models.py
@@ -4,19 +4,33 @@ from django.db import models
 class State(models.Model):
     state = models.CharField(max_length=128)
 
+    def __repr__(self):
+        return self.state
+
+    def __str__(self):
+        return repr(self)
+
 
 class PostCode(models.Model):
     code = models.CharField(max_length=128)
+    town = models.CharField(max_length=128)
+
+    def __repr__(self):
+        return "%s (%s)" % (self.code, self.town)
+
+    def __str__(self):
+        return repr(self)
 
 
-# Create your models here.
-class Address(models.Model):
-    state = models.ForeignKey(State)
-    postcode = models.ForeignKey(PostCode)
-
-
-class Person(models.Model):
-    address = models.ForeignKey(Address)
+class Entry(models.Model):
     first_name = models.CharField(max_length=128)
     last_name = models.CharField(max_length=128)
     mobile = models.CharField(max_length=128)
+
+
+class Address(models.Model):
+    state = models.ForeignKey(State)
+    postcode = models.ForeignKey(PostCode)
+    street = models.CharField(max_length=128)
+    entry = models.ForeignKey(Entry)
+
